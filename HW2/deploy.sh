82bbc4e58e2a314f0b3867dd3dc60e804bf41272
Chen <chen@ChendeMBP.wind3.hub>
2023-05-27 18:02:21 +0200

    add css html doc

--- HW2/deploy.sh
+++
@@ -1,6 +0,0 @@
-# Change the path to tomcat to your actual path
-mvn clean install
-rm -rf /opt/homebrew/opt/tomcat/libexec/webapps/workflix-1.0.war
-rm -rf /opt/homebrew/opt/tomcat/libexec/webapps/workflix-1.0
-cp target/workflix-1.0.war /opt/homebrew/opt/tomcat/libexec/webapps
-echo "Deployed to Tomcat"
