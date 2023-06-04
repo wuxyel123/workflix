33b322af8f993319897ada6541d0a5c4f981e140
Chen <chen@ChendeMBP.wind3.hub>
2023-06-04 18:55:09 +0200

    setting

--- HW2/src/main/webapp/js/Usersetting.js
+++ HW2/src/main/webapp/js/Usersetting.js
@@ -1,160 +1,83 @@
-var contextPath = "http://127.0.0.1:8080/workflix-1.0";
-
-document.addEventListener("DOMContentLoaded", function (event) {
-  loadTemplate();
-});
-
-function loadTemplate() {
-  var headerUrl = new URL(
-    contextPath + "/src/main/webapp/html/Reusable-snippets/header.html"
-  );
-  var sidebarUrl = new URL(
-    contextPath + "/src/main/webapp/html/Reusable-snippets/sidebar.html"
-  );
-  var footerUrl = new URL(
-    contextPath + "/src/main/webapp/html/Reusable-snippets/footer.html"
-  );
-  sendGenericGetRequest(headerUrl, loadHeader);
-  sendGenericGetRequest(sidebarUrl, loadSidebar);
-  sendGenericGetRequest(footerUrl, loadFooter);
+function validateEmail(email) {
+  // for check email
+  var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailPattern.test(email);
 }
 
-function sendGenericGetRequest(url, callback) {
-  var httpRequest = new XMLHttpRequest();
-
-  if (!httpRequest) {
-    alert("Cannot create an XMLHTTP instance");
-    return false;
-  }
-  httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status == 200) {
-        callback(httpRequest.responseText);
-      } else {
-        console.log(req.responseText);
-        alert("problem processing the request");
-      }
-    }
-  };
-
-  httpRequest.open("GET", url);
-  httpRequest.send();
+// check password
+function validatePassword(password, confirmPassword) {
+  return password === confirmPassword;
 }
 
-function loadSidebar(data) {
-  var loggedIn = sessionStorage.getItem("loggedIn");
-  var userAuthorization = sessionStorage.getItem("userRole");
+document.addEventListener("DOMContentLoaded", function () {
+  var userForm = document.getElementById("userForm");
+  var saveButton = document.getElementById("saveButton");
 
-  document.getElementById("sidebar-area").innerHTML = data;
-  // document.getElementById("template-link-button").setAttribute("href", contextPath + "/src/main/webapp/html/Components/template.html");
-  // document.getElementById("analytics-link-button").setAttribute("href", contextPath + "/src/main/webapp/html/Components/analytics.html");
+  // get user info
+  getUserData();
 
-  var sidebarButton = document.getElementById("sidebar-toggle-button");
+  //  Listening for form submission events
+  userForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  sidebarButton.addEventListener("click", function () {
-    var sideBar = document.getElementById("sidebar-area");
-    console.log(sideBar.style.left);
-    var currentLeft = parseInt(sideBar.getAttribute("data-left")) || 0;
-    var newLeft = currentLeft === 0 ? -120 : 0; // Toggle between 0 and 200
-    sideBar.style.left = newLeft + "px";
-    sideBar.setAttribute("data-left", newLeft);
+    // Get form data
+    var username = document.getElementById("username").value;
+    var name = document.getElementById("name").value;
+    var surname = document.getElementById("surname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    // update user data
+    updateUserData(username, name, surname, email, password);
   });
 
-  if (loggedIn) {
-    if (
-      userAuthorization == "manager" ||
-      userAuthorization == "user" ||
-      userAuthorization == "editor"
-    ) {
-      list = document.getElementsByClassName("manager");
-      for (var i = 0; i < list.length; i++) {
-        list[i].classList.add("d-block");
-        list[i].classList.remove("d-none");
-      }
-      if (userAuthorization == "user" || userAuthorization == "editor") {
-        list = document.getElementsByClassName("editor");
-        for (var i = 0; i < list.length; i++) {
-          list[i].classList.add("d-block");
-          list[i].classList.remove("d-none");
+  function getUserData() {
+    fetch("/user/getData")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        //Populate the table with the obtained user data
+        document.getElementById("username").value = data.username;
+        document.getElementById("surname").value = data.first_name;
+        document.getElementById("name").value = data.last_name;
+        document.getElementById("email").value = data.email;
+        document.getElementById("description").value = data.description;
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  }
+
+  // update user info
+  function updateUserData(username, name, surname, email, password) {
+    var userData = {
+      user_id: useId,
+      username: username,
+      last_name: name,
+      first_name: surname,
+      email: email,
+      password: password,
+      description: description,
+    };
+
+    // update requests
+    fetch("/user/update/" + useId, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then(function (response) {
+        if (response.ok) {
+          console.log("User data updated successfully");
+        } else {
+          console.error("Failed to update user data");
         }
-        if (userAuthorization == "user") {
-          list = document.getElementsByClassName("user");
-          for (var i = 0; i < list.length; i++) {
-            list[i].classList.add("d-block");
-            list[i].classList.remove("d-none");
-          }
-        }
-      }
-    }
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
   }
-  if (!loggedIn) {
-    list = document.getElementsByClassName("manager");
-    for (var i = 0; i < list.length; i++) {
-      list[i].classList.add("d-none");
-      list[i].classList.remove("d-block");
-    }
-
-    list = document.getElementsByClassName("user");
-    for (var i = 0; i < list.length; i++) {
-      list[i].classList.add("d-none");
-      list[i].classList.remove("d-block");
-    }
-
-    list = document.getElementsByClassName("editor");
-    for (var i = 0; i < list.length; i++) {
-      list[i].classList.add("d-none");
-      list[i].classList.remove("d-block");
-    }
-  }
-
-  // document.getElementById("drop-down-manager-menu").addEventListener('click', function (event) {
-
-  //     if (document.getElementById("icon-manager-menu").classList.contains("fa-angle-down")) {
-  //         document.getElementById('icon-manager-menu').classList.add('fa-angle-up');
-  //         document.getElementById('icon-manager-menu').classList.remove('fa-angle-down');
-  //     } else {
-  //         document.getElementById('icon-manager-menu').classList.remove('fa-angle-up');
-  //         document.getElementById('icon-manager-menu').classList.add('fa-angle-down');
-  //     }
-  // });
-}
-
-function loadHeader(data) {
-  var loggedIn = sessionStorage.getItem("loggedIn");
-  var userEmail = sessionStorage.getItem("userEmail");
-
-  // document.getElementById("header-area").innerHTML = data;
-  // document.getElementById("logo-button").setAttribute("href", contextPath + "/src/main/webapp/html/LandingPage.html");
-  // document.getElementById("logout-button").setAttribute("href", contextPath + "/user/logout/?operation=logout");
-  // document.getElementById("login-button").setAttribute("href", contextPath + "/jsp/login.jsp");
-  // document.getElementById("register-button").setAttribute("href", contextPath + "/jsp/register.jsp");
-
-  if (loggedIn) {
-    document.getElementById("user-email").innerHTML = userEmail;
-
-    list = document.getElementsByClassName("unlogged");
-    for (var i = 0; i < list.length; i++) {
-      list[i].setAttribute("class", "unlogged d-none");
-    }
-
-    list = document.getElementsByClassName("logged");
-    for (var i = 0; i < list.length; i++) {
-      list[i].setAttribute("class", "logged d-block");
-    }
-  }
-  if (!loggedIn) {
-    list = document.getElementsByClassName("logged");
-    for (var i = 0; i < list.length; i++) {
-      list[i].setAttribute("class", "logged d-none");
-    }
-
-    list = document.getElementsByClassName("unlogged");
-    for (var i = 0; i < list.length; i++) {
-      list[i].setAttribute("class", "unlogged d-block");
-    }
-  }
-}
-
-function loadFooter(data) {
-  document.getElementById("footer-area").innerHTML = data;
-}
+});
--- HW2/src/main/webapp/js/board-header.js
+++ HW2/src/main/webapp/js/board-header.js
@@ -1,115 +1,15 @@
-$('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-    }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+$(document).ready(function () {
+  // log out
+  $("#logoutButton").on("click", function () {
+    $("#logoutModal").modal("show");
+  });
+
+  $("#cancelLogoutButton").on("click", function () {
+    $("#logoutModal").modal("hide");
+  });
+
+  $("#confirmLogoutButton").on("click", function () {
+    localStorage.removeItem("loggedInUser");
+    window.location.href = "login.html";
+  });
 });
-class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
-    connectedCallback() {
-        this.innerHTML = `
-        <style>
-          header{
-            position: absolute;
-            z-index: 999999;
-            width: 100%;
-            background: #ffff;
-          }
-          nav.navbar.navbar-expand-lg.bg-body-tertiary {
-            background: #4e4c4c05;
-            border: 1px solid #8080806b;
-            padding: 1%;
-          }
-          .navbar-brand {
-            background-image: url(./logo.png);
-            width: 100px;
-            height: 50px;
-            background-position: center;
-            background-size: contain;
-          }
-          ul.navbar-nav.me-auto.mb-2.mb-lg-0 {
-            margin-left: 5%;
-            width: 50%;
-          }
-          .settings {
-            width: 100%;
-          }
-          span.user-settings {
-            float: right;
-            position: relative;
-            margin-right: 2%;
-            background: #80808033;
-            /* padding: 1%; */
-            border-radius: 100pc;
-            width: 35px;
-            height: 35px;
-            text-align: center;
-            padding-top: .50%;
-            color: #0b0b43;
-            cursor: pointer;
-          }
-        </style>
-        <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <i class="fa-brands fa-microblog"></i> 
-
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" style="color: black;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Workspaces <i class="fa-solid fa-magnifying-glass"></i>
-                    </a>
-                    <ul class="dropdown-menu">
-                    <li>
-                        <a class="dropdown-item" style="color: black;" href="#"><i class="fa-solid fa-user-group"></i> Friends Updates</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" style="color: black;" href="#"><i class="fa-solid fa-calendar"></i> Events near you</a>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                        <a class="dropdown-item" style="color: black;" href="#"><i class="fa-solid fa-ticket"></i> Buy a Ticket</a>
-                    </li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" style="color: black;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Templates <i class="fa-solid fa-magnifying-glass"></i>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                        <a class="dropdown-item" style="color: black;" href="#"><i class="fa-solid fa-user-group"></i> Friends Updates</a>
-                        </li>
-                        <li>
-                        <a class="dropdown-item" style="color: black;" href="#"><i class="fa-solid fa-calendar"></i> Events near you</a>
-                        </li>
-                        <li>
-                        <hr class="dropdown-divider" />
-                        </li>
-                        <li>
-                        <a class="dropdown-item" style="color: black;" href="#"><i class="fa-solid fa-ticket"></i> Buy a Ticket</a>
-                        </li>
-                    </ul>
-                    </li>
-                </ul>
-                <div class="settings">
-                    <span class="user-settings">EE</span>
-                </div>
-            </div>
-            </div>
-        </nav>
-
-        </header>
-        `;
-      }
-}
-customElements.define('header-component', Header);
